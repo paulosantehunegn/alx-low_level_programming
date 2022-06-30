67643ae8a6f690a7bfafff15e9c011d0cc5350cb
paulosantehunegn <paulosantu79@gmail.com>
2022-06-30 13:56:37 -0700

    Task 4 fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
  * main - entry point
  *
  * return: 0 after printing function.
- i*/
+ **/
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+             
+          	return (0);
 }
