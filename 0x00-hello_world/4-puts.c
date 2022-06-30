db6f6b88f8167ae2a324086d97390180423a4641
paulosantehunegn <paulosantu79@gmail.com>
2022-06-30 14:43:05 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'the program's description'
+ * Description: 'the program's description
  * Return: Always 0 (Success) 
  **/
 int main(void)
