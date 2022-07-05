ed29a7b90944f7e03ecefa4804780a7bc0135cab
paulosantehunegn <paulosantu79@gmail.com>
2022-07-05 08:46:25 -0700

    Task 0 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints to string
+ * Description: Prints positive, negative or zero
+ * Return: 0 Always
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+
+	return (0);
+}
+
