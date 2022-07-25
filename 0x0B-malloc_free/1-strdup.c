a158f749c39319661ac3a8f1831ae73448721995
paulosantehunegn <paulosantu79@gmail.com>
2022-07-25 15:51:18 -0700

    Task 1 corrected

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,6 +14,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
+		;
 		strout = (char *)malloc(sizeof(char) * (i + 1));
 	if (strout == NULL)
 		return (NULL);
